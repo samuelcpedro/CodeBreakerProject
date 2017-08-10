5eade3d8609229fdce681eca1c06063799cfac90
Samuel Pedro <samuelcpedro@gmail.com>
2017-08-10 20:16:37 +0000

    Create setHiddenFields Function
    Create a function named setHiddenFields that sets the answer variable equal to a randomly generated whole number between 0 and 9999.
    
    Hint: Math.random() can be used to randomly generate a number between 0 and 1 (up to 18 decimal points) and Math.floor(input) can be used to round down to the nearest whole number.

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,7 @@ function guess() {
 }
 
 //implement new functions here
+function setHiddenFields (answer){
+	//Vou guardar a parte inteira do valor produzido pelo random multiplicado por 10000
+	answer = Math.floor(Math.random() * 10000); //The maximum is exclusive and the minimum is inclusive
+}
