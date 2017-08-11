6a5eb1207444da598951553d6c06b19851189dca
Samuel Pedro <samuelcpedro@gmail.com>
2017-08-11 19:06:11 +0000

    teste

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,9 +7,10 @@ function guess() {
 }
 
 //implement new functions here
-var setHiddenFields = function(){
+function setHiddenFields(){
 	//Vou guardar a parte inteira do valor produzido pelo random multiplicado por 10000
 	answer = Math.floor(Math.random() * 10000); //The maximum is exclusive and the minimum is inclusive
 	//vou repetir x vezes a string "0" até ter o a string answer ter tamanho 4
-	return answer = "0".repeat(4-String(answer).length) + answer.toString(10);
+	//console.log(answer);
+	//return answer = "0".repeat(4-String(answer).length) + answer.toString(10);
 };
