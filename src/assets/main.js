dc7d835dd099a9bc6252768d503dc87a9b3d5be1
Samuel Pedro <samuelcpedro@gmail.com>
2017-08-11 19:39:23 +0000

    ly set the answer and attempt hidden inputs when they aren't already set 1
    Call the setHiddenFields function in the body of the guess function, but also write some logic so that it's only called when answer and attempt haven't already been set.
    
    Hint: we can use an if condition to only run our code when answer or attempt is empty ('').

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,12 +2,16 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+	let input = document.getElementById('user-guess');
+	//add functionality to guess function here
+	if (answer.value.length == 0 && attempt.value.length == 0) {
+		setHiddenFields ();
+	}
 }
 
 //implement new functions here
 var setHiddenFields = function(){
+	attempt = 0;
 	//Vou guardar a parte inteira do valor produzido pelo random multiplicado por 10000
 	var aux  = Math.floor(Math.random() * 10000); //The maximum is exclusive and the minimum is inclusive
 	//vou repetir x vezes a string "0" até ter o a string answer ter tamanho 4
